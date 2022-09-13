e9fe794964e805ab3aca459d133c906643ea0ea3
KBaron34 <113381057+KBaron34@users.noreply.github.com>
2022-09-13 18:53:40 +0300

    Add files via upload

---
+++ Roman to Integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        result = 0
+        integers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for i in range(len(s)):
+            if i + 1 < len(s) and integers[s[i]] < integers[s[i+1]]:
+                result -= integers[s[i]]
+            else:
+                result += integers[s[i]]
+        return result
+
+
+num = Solution()
+num.romanToInt(input())
